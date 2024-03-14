4dc214b20237edeba492d810e58cb35bb5643a92
2010010629 <abdulwahid120600@gmail.com>
2024-03-14 16:01:16 +0800

    tries job unfinished

--- app/Jobs/CheckMonitoringJob.php
+++ app/Jobs/CheckMonitoringJob.php
@@ -32,15 +32,16 @@ class CheckMonitoringJob implements ShouldQueue
     public function handle(): void
     {
         // Lakukan HTTP request ke website yang ingin dimonitoring
-        $start = microtime(true);
+        $start = microtime(true); // waktu mulai akses website
         $response = Http::get($this->monitoring['url']);
-        $end = microtime(true);
+        $end = microtime(true); // waktu selesai akses website
         $response_time = round($end - $start, 2); // waktu response
         $status = $response->status(); // status kode
-        $created_at = date('Y-m-d H:i:s');
-        $updated_at = date('Y-m-d H:i:s');
+        $created_at = now();
+        $updated_at = now();
         $monitoring_id = $this->monitoring['id'];
         $user_id = $this->monitoring['user_id'];
+        $arr_success = range(200, 299);
         // Menyimpan hasil monitoring ke tabel result monitoring
         $monitoring = [
             'response_time' => $response_time,
@@ -50,19 +51,30 @@ class CheckMonitoringJob implements ShouldQueue
             'monitoring_id' => $monitoring_id,
             'user_id' => $user_id,
         ];
-        $save = Result::create($monitoring);
-        // tampilkan pesan jika berhasil di simpan
-        if ($save) {
-            Log::info("Response Time: {$response_time} seconds, Status: {$status}");
-        } else {
-            Log::info("Website Down!");
-        }
+        $save_result = Result::create($monitoring);
 
         // menghitung rata rata response_time per id
-        $avg = Result::where('monitoring_id', $monitoring_id)->sum('response_time') / Result::where('monitoring_id', $monitoring_id)->count();
+        $avg_response = Result::where('monitoring_id', $monitoring_id)->sum('response_time') / Result::where('monitoring_id', $monitoring_id)->count();
         // simpan rata rata response time ke tabel monitoring
         $monitor_id = Monitoring::findOrFail($monitoring_id);
-        $monitor_id->avg_response_time = $avg;
+        $monitor_id->avg_response_time = $avg_response;
         $monitor_id->save();
+
+        // cek status code di result monitoring
+        try {
+            $getResult = Result::findOrFail($save_result->id);
+            if (!in_array($getResult->status_code, $arr_success)) {
+                // lakukan pengulangan sebanyak tries yang di input user
+                for ($i = 0; $i < $monitor_id->tries; $i++) {
+                    dispatch(new CheckMonitoringJob($this->monitoring));
+                }
+                if (!in_array($getResult->status_code, $arr_success)) {
+                    // jika masih gagal status code nya maka kirim email job
+                    Log::info('Pesan akan dikirim melalui email job');
+                }
+            }
+        } catch (\Exception $e) {
+            Log::error('Failed to find Result: ' . $e->getMessage());
+        }
     }
 }
